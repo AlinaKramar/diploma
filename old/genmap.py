ddab6ad4fd9ea37c22d8467338ba45d0c826a159
Alina Kramar <kramar.alina@gmail.com>
2014-05-30 02:48:48 +0400

    genmap

--- old/genmap.py
+++ old/genmap.py
@@ -5,12 +5,12 @@
 
 '''
 
-NAME = 'c:\\python27\\Lib\\chr1000.gen'
+NAME = 'chr1000.gen'
 
 def read_line(f):
     for l in f:
         if l != '\n':
-            return l.strip()   
+            return l.strip()
 
 #
 #    Open and parse the .GEN file
@@ -21,24 +21,24 @@ def open_file(name):
         read_line(f)                # number of FAMILIES - not used, assumed 1
         M = int(read_line(f))       # number of loci
         locs = read_line(f).split(' ')  # names of loci
-        
+
         pedigree = []               # data array for the whole pedigree
         read_line(f)                # read family number
         number = int(read_line(f))  # number of species
- 
+
         for i in range(number):                  # READ FAMILY
             specie = read_line(f).split(' ')     # read species
             allels = list(int(a) for a in read_line(f).split(' '))
             specie.append(allels)
-            pedigree.append(specie)       
-        
-        f.close()    
+            pedigree.append(specie)
+
+        f.close()
         return M, number, locs, pedigree
     except:
         if f:
             f.close()
         return 0, 0, None, None
-    
+
 #
 #    Reveal the gametes
 #
@@ -54,19 +54,19 @@ def reveal_gametes(M, number, pedigree):
             else:
                 g1[i] = g2[i] = 0
             g1[M] = g2[M] = 0                       # we don't know the parent for the gamete yet
-            
+
         # find parents
         parents = []
         for p in pedigree:
             if p[0] == s[1] or p[0] == s[2]:
                 parents.append(p)
         if len(parents) == 0:
-            s.append(g1)       
-            s.append(g2)       
+            s.append(g1)
+            s.append(g2)
             continue
-        
+
         # cycle through all loci
-        p = parents[0] 
+        p = parents[0]
         for i in range(M):
             if g1[i] != 0:                  # we already know this locus
                 continue
@@ -76,39 +76,39 @@ def reveal_gametes(M, number, pedigree):
                 g2[i] = 3 - p[4][2 * i]     # 2->1, 1->2
                 if len(parents) == 2:
                     g2[M] = parents[1][0]   # the parent for the gamete
-                continue 
+                continue
             else:
                 if len(parents) == 2:       # look at the other parent
                     m = parents[1]
                     if m[4][2 * i] == m[4][2 * i + 1] and m[4][2 * i] != 0:
                         g2[i] = m[4][2 * i]
                         g2[M] = m[0]
-                        g1[i] = 3 - m[4][2 * i]     
+                        g1[i] = 3 - m[4][2 * i]
                         g1[M] = p[0]
-		
+
 	#
-	#  31.05.13  Sysoev. It is useful to assign equal gametes to the parents, because homozygota child of 
+	#  31.05.13  Sysoev. It is useful to assign equal gametes to the parents, because homozygota child of
 	#  heterozygota parent can be useful for further data retrival
 	#
 	if g1[M] == 0:
 	    g1[M] = parents[0][0]
 	if g2[M] == 0 and len(parents) == 2:
-            g2[M] = parents[1][0]		
+            g2[M] = parents[1][0]
 
 	# store gamets
-        s.append(g1)       
-        s.append(g2)       
+        s.append(g1)
+        s.append(g2)
     return
 
 
-	
+
 #
 #    Process the gametes
 #	 M - number of loci
 #	 pedigree - species data with the phase revealed
 #	 stat - boolean flag. If true then we try to estimate recombination fraction statistically among the children of one specie
 #
-def process_gametes(M, pedigree, stat = True):        
+def process_gametes(M, pedigree, stat = True):
     # init the CIS - TRANS estimation matrix
     cistrans = []
     for i in range(M):
@@ -116,13 +116,13 @@ def process_gametes(M, pedigree, stat = True):
         cistrans.append(row)
 
     # cycle through the species
-    for s in pedigree:         
+    for s in pedigree:
         # find children
         children = []
         for ch in pedigree:
             if ch[1] == s[0] or ch[2] == s[0]:
                 children.append(ch)
-                
+
         if len(children) == 0:              # no children - no meioses
             continue
 
@@ -131,13 +131,13 @@ def process_gametes(M, pedigree, stat = True):
             a1 = s[4][i * 2]
             a2 = s[4][i * 2 + 1]
             if a1 == a2:                    # homozygota
-                continue                    
+                continue
             for j in range(i + 1, M):
                 b1 = s[4][j * 2]
                 b2 = s[4][j * 2 + 1]
                 if b1 == b2:                # homozygota
                     continue
-                						
+
 		type1 = type2 = rec = nonrec = 0
 
                 # cycle through the children
@@ -151,13 +151,13 @@ def process_gametes(M, pedigree, stat = True):
                         continue
                     if gamete[i] == 0 or gamete[j] == 0:    # the meiosis was uninformative on these loci
                         continue
-						
+
 		    # gather the statistical info
                     if gamete[i] == gamete[j]:
 			type1 += 1		# AB or ab
 		    else:
 			type2 += 1		# Ab or aB
-					
+
 		    # gather the reliable info
                     if (gamete[i] == s[5][i] and gamete[j] == s[6][j]) or (gamete[i] == s[6][i] and gamete[j] == s[5][j]):
                         rec += 1	        # RECOMBINATION
@@ -169,7 +169,7 @@ def process_gametes(M, pedigree, stat = True):
 			# print rec, nonrec, type1, type2
 			nonrec = max(type1, type2)
 			rec = min(type1, type2)
-					
+
                 cistrans[i][j][0] += rec
                 cistrans[i][j][1] += nonrec
 
@@ -181,7 +181,7 @@ def process_gametes(M, pedigree, stat = True):
 def form_cluster(M, matrix):
     best_cluster = None
     best_cluster_len = 0
-    
+
     #
     # try to form cluster from every node, than choose best
     #
@@ -191,8 +191,8 @@ def form_cluster(M, matrix):
         locus = l
         temp[l] = -1		# mark as used
         cluster.append(locus)
-        
-        # find the first neighbor 
+
+        # find the first neighbor
         neighbor = None
         dist = 1
         for i in range(M):
@@ -208,7 +208,7 @@ def form_cluster(M, matrix):
         cluster.append(neighbor)
         temp[neighbor] = -1
 
-        # find the second neighbor 
+        # find the second neighbor
         neighbor2 = None
         dist = 1
         for i in range(M):
@@ -221,7 +221,7 @@ def form_cluster(M, matrix):
                 best_cluster = cluster
             continue
         total_length = matrix[neighbor][neighbor2]
-        
+
         # build the chain
         while True:
             locus = neighbor
@@ -236,7 +236,7 @@ def form_cluster(M, matrix):
                     best_cluster_len = len(cluster)
                     best_cluster = cluster
                 break
-            
+
             cluster.append(neighbor)
             temp[neighbor] = -1
             #
@@ -250,8 +250,8 @@ def form_cluster(M, matrix):
     return best_cluster
 
 #
-#  We've formed the cluster, but oops.. some loci are not in it. Inserting them... 
-#	
+#  We've formed the cluster, but oops.. some loci are not in it. Inserting them...
+#
 def insert_locus(cluster, locus, matrix):
     # find the closest neighbor
     neighbor1 = None
@@ -273,14 +273,14 @@ def insert_locus(cluster, locus, matrix):
         else:
             cluster.insert(neighbor1, locus)		# no, before the last
         return cluster
-    
+
     # it is in the middle. But what is this neighbor? Right or Left?
     if matrix[locus][cluster[neighbor1 - 1]] < matrix[cluster[neighbor1]][cluster[neighbor1 - 1]]:
         cluster.insert(neighbor1, locus)
     else:
         cluster.insert(neighbor1 + 1, locus)
-    return cluster    
-        
+    return cluster
+
 #
 #   Given the recombinations, calculate the fractions
 #
@@ -291,16 +291,16 @@ def process_matrix(M, matrix, number):
         row = []
         for j in range(M):
             if j == i:
-                recombinants = 0 
+                recombinants = 0
                 nonrecombinants = 1
-            else: 
+            else:
                 if j < i + 1:		# initial matrix is triangle (see how we form it). Fracs matrix should be full
-                    recombinants = matrix[j][i][0] 
+                    recombinants = matrix[j][i][0]
                     nonrecombinants = matrix[j][i][1]
-                else: 
-                    recombinants = matrix[i][j][0] 
+                else:
+                    recombinants = matrix[i][j][0]
                     nonrecombinants = matrix[i][j][1]
-                
+
             if recombinants != 0 or nonrecombinants != 0:
                 row.append(float(recombinants) / (recombinants + nonrecombinants))
             else:
@@ -309,6 +309,8 @@ def process_matrix(M, matrix, number):
         fracs.append(row)
 
     return fracs
+
+
 
 #
 #    process the pedigree. Main function in the module
@@ -331,13 +333,13 @@ def process_pedigree(file_name, order = [], stat = True):
         cluster = form_cluster(M, fracs)
     else:
         cluster = order
-        
+
     for l in range(M):
         try:
             index = cluster.index(l)
         except:
             cluster = insert_locus(cluster, l, fracs)
-		
+
     for i in range(len(cluster)):
         if i < len(cluster) - 1:
             print locs[cluster[i]], '   ', fracs[cluster[i]][cluster[i + 1]]
@@ -345,3 +347,5 @@ def process_pedigree(file_name, order = [], stat = True):
             print locs[cluster[i]]
 
     return cluster, fracs
+
+process_pedigree(NAME, [], False)
